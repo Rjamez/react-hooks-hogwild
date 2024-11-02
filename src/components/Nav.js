3b56900994d69cabee2f318daf261eca828bcba4
Rjamez <robin.adhola.student@moringaschool.com>
2024-11-02 21:27:20 +0300

    src

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,20 @@
 import React from "react";
-import piggy from "../assets/porco.png";
+import piggy from "../assets/porco.png"; // Ensure this path is correct
 
 const Nav = () => {
-	return (
-		<nav>
-			<div className="navWrapper">
-				<span className="headerText">HogWild</span>
-				<div className="TwirlyPig">
-					<img src={piggy} className="App-logo" alt="piggy" />
-				</div>
-				<span className="normalText">
-					A React App for County Fair Hog Fans
-				</span>
-			</div>
-		</nav>
-	);
+    return (
+        <nav>
+            <div className="navWrapper">
+                <span className="headerText">HogWild</span>
+                <div className="TwirlyPig">
+                    <img src={piggy} className="App-logo" alt="Piggy" />
+                </div>
+                <span className="normalText">
+                    A React App for County Fair Hog Fans
+                </span>
+            </div>
+        </nav>
+    );
 };
 
 export default Nav;
